d9f7d8cf1a7c8e21d9152ecc999cf96e363cb241
Malekus <malekus@outlook.fr>
2018-09-24 13:55:29 +0200

    tp1 done

--- tp1.py
+++ tp1.py
@@ -53,7 +53,7 @@ print mnist.data.size
 print mnist.data.shape[0]
 
 #afficher le numero de chaque classse
-
+print "le numero de classe"
 
 #afficher la moyenne pour chaque variable MNIST
 print mnist.data.mean(0)
@@ -76,9 +76,6 @@ print np.unique(mnist.target)
 #generer des donnees 1000 donnes=n_samples, 2 variables=n_features, 4 groupes=centers
 data, label = datasets.make_blobs(n_samples=500, n_features=2, centers=4)
 
-
-
-
 #creer une figure vide
 plt.figure()
 
@@ -89,14 +86,7 @@ plt.xlim(-15, 15)
 plt.ylim(-15, 15)
 
 #utilisation du scatter
-plt.scatter(x=[x for index, x in enumerate(data[:,0]) if label[index] == 0],
-            y=[y for index, y in enumerate(data[:,1]) if label[index] == 0], label="rouge", color="r")
-plt.scatter(x=[x for index, x in enumerate(data[:,0]) if label[index] == 1],
-            y=[y for index, y in enumerate(data[:,1]) if label[index] == 1], label="bleu", color="b")
-plt.scatter(x=[x for index, x in enumerate(data[:,0]) if label[index] == 2],
-            y=[y for index, y in enumerate(data[:,1]) if label[index] == 2], label="vert", color="g")
-plt.scatter(x=[x for index, x in enumerate(data[:,0]) if label[index] == 3],
-            y=[y for index, y in enumerate(data[:,1]) if label[index] == 3], label="jaune", color="y")
+plt.scatter(x=data[:,0], y=data[:,1],c=label)
 
 #titre du plot
 plt.title("1000 donnees de deux variables\nreparties en 4 groupes")
@@ -119,12 +109,7 @@ c, d = datasets.make_blobs(n_samples=250, n_features=2, centers=3)
 plt.figure()
 plt.xlim(-15, 15)
 plt.ylim(-15, 15)
-plt.scatter(x=[x for index, x in enumerate(a[:,0]) if b[index] == 0],
-            y=[y for index, y in enumerate(a[:,1]) if b[index] == 0],
-            label="rouge", color="r")
-plt.scatter(x=[x for index, x in enumerate(a[:,0]) if b[index] == 1],
-            y=[y for index, y in enumerate(a[:,1]) if b[index] == 1],
-            label="bleu", color="b")
+plt.scatter(x=a[:,0], y=a[:,1], c=b)
 plt.title("100 donnees de deux variables\nreparties en 2 groupes")
 plt.xlabel("valeur de x")
 plt.ylabel("valeur de y")
@@ -134,15 +119,8 @@ plt.show()
 plt.figure()
 plt.xlim(-15, 15)
 plt.ylim(-15, 15)
-plt.scatter(x=[x for index, x in enumerate(c[:,0]) if d[index] == 0],
-            y=[y for index, y in enumerate(c[:,1]) if d[index] == 0],
-            label="rouge", color="r")
-plt.scatter(x=[x for index, x in enumerate(c[:,0]) if d[index] == 1],
-            y=[y for index, y in enumerate(c[:,1]) if d[index] == 1],
-            label="bleu", color="b")
-plt.scatter(x=[x for index, x in enumerate(c[:,0]) if d[index] == 2],
-            y=[y for index, y in enumerate(c[:,1]) if d[index] == 2],
-            label="vert", color="g")
+plt.scatter(x=c[:,0],
+            y=c[:,1],c=d)
 plt.title("500 donnees de deux variables\nreparties en 3 groupes")
 plt.xlabel("valeur de x")
 plt.ylabel("valeur de y")
@@ -156,15 +134,7 @@ lastData, lastLabel = np.vstack((a, c)), np.hstack((b, d))
 plt.figure()
 plt.xlim(-15, 15)
 plt.ylim(-15, 15)
-plt.scatter(x=[x for index, x in enumerate(lastData[:,0]) if lastLabel[index] == 0],
-            y=[y for index, y in enumerate(lastData[:,1]) if lastLabel[index] == 0],
-            label="rouge", color="r")
-plt.scatter(x=[x for index, x in enumerate(lastData[:,0]) if lastLabel[index] == 1],
-            y=[y for index, y in enumerate(lastData[:,1]) if lastLabel[index] == 1],
-            label="bleu", color="b")
-plt.scatter(x=[x for index, x in enumerate(lastData[:,0]) if lastLabel[index] == 2],
-            y=[y for index, y in enumerate(lastData[:,1]) if lastLabel[index] == 2],
-            label="vert", color="g")
+plt.scatter(x=lastData[:,0], y=lastData[:,1], c=lastLabel)
 plt.title("Concatenation des donnees")
 plt.xlabel("valeur de x")
 plt.ylabel("valeur de y")
