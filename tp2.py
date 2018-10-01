10cce5f234c8c4e652bd58a7ed528dc188f2f92c
Malekus <malekus@outlook.fr>
2018-10-01 17:17:28 +0200

    tp2 done

--- tp2.py
+++ tp2.py
@@ -26,9 +26,6 @@ print x2
 scaler = preprocessing.MinMaxScaler()
 ppx2 = scaler.fit(x2)
 
-print "toto"
-
-
 # calculer la valeur min revient a calculer le moyenne de chaque variable
 # calculer la valeur max revient a calculer le varia,ce de chaque variable
 print np.min(scaler.transform(x2), axis=0)
@@ -40,19 +37,36 @@ m = 0
 plt.figure(1)
 for ligne in range(0, len(iris.feature_names)):
     for colonne in range(0, len(iris.feature_names)):
-        if ligne != colonne and ligne <= colonne: 
-            m = m + 1     
+        if ligne != colonne and ligne <= colonne:
+            m = m + 1
             plt.subplot(2, 3, m)
-            plt.scatter(x=iris.data[:,ligne], y=iris.data[:,colonne], c=iris.target)
-            plt.title("Ligne "+ str(ligne) + " Colonne " + str(colonne))
-        
+            plt.scatter(x=iris.data[:,ligne], y=iris.data[:,colonne], c=iris.target, alpha=0.8)
+            plt.title("Figure : Ligne "+ str(ligne) + " Colonne " + str(colonne))
+
+plt.show()
+#Figure Ligne 0 Colonne 3
+
 
 from sklearn.decomposition import PCA
+from sklearn.lda import LDA
+
+
 
 pca = PCA(n_components=2)
-print pca.fit(iris.data).transform(iris.data).shape
-plt.figure(2)
-plt.scatter(pca.fit(iris.data).transform(iris.data)[:,0], pca.fit(iris.data).transform(iris.data)[:,1], c=iris.target)
+
+irisPCA = pca.fit(iris.data).transform(iris.data)
+irisLDA = LDA().fit(iris.data, iris.target).transform(iris.data)
+
+
+plt.figure("Figure PCA/LDA de iris data")
+plt.subplot(1, 3, 1)
+plt.scatter(irisPCA[:,0], irisPCA[:,1], c=iris.target, alpha=0.8)
+plt.legend()
+plt.subplot(1, 3, 2)
+plt.scatter(irisLDA[:,0], irisLDA[:,1], c=iris.target, alpha=0.8)
+plt.subplot(1, 3, 3)
+plt.scatter(irisLDA[:,0], irisLDA[:,1], c=LDA().fit(iris.data, iris.target).predict(iris.data), alpha=0.8)
+plt.legend()
 plt.show()
 
-
+print(sum(LDA().fit(iris.data, iris.target).predict(iris.data) == iris.target) / (iris.data.shape[0] + 0.0))
