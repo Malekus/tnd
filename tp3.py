9f94cb92c11c4de175e5d64c6bc91d38830bcbd0
Malekus <malekus@outlook.fr>
2018-10-17 08:24:31 +0200

    done

--- tp3.py
+++ tp3.py
@@ -3,17 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics.pairwise import euclidean_distances
-
+from sklearn.naive_bayes import GaussianNB
+#  np.set_printoptions(threshold=np.nan)
 iris = datasets.load_iris()
 x = iris.data
 y = iris.target
 
 
-
-
-def ppv(x, y, K):
+def ppv(x, y, voisin):
     r = []
-    neight = KNeighborsClassifier(n_neighbors=K)
+    neight = KNeighborsClassifier(n_neighbors=voisin)
     neight.fit(x,y)
     for data in x:
         p = metrics.pairwise.euclidean_distances(data.reshape(1,-1), x)
@@ -26,6 +25,22 @@ def ppv(x, y, K):
 
 predictData, taux =  ppv(x, y, 1)
 
+print predictData, taux
 
 
-print taux
+"""
+def CBN(x, y):
+    r = []
+    clf = GaussianNB
+    clf.fit(x, y)
+    for data in x:
+        print data
+        
+
+
+for k in np.unique(iris.target):
+    k_len =  np.where(iris.target==k)
+    print iris.data[k_len,:].mean(1)
+    #print iris.data[k_len,:].mean(1)
+    #print np.asarray(np.where(iris.target==k)).size
+"""     
