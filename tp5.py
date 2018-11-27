bf9fbbc0eb13aa9feaa961d5d95a5a9e74165366
ChefDeProjet <Malekus@localhost.localdomain>
2018-11-27 23:29:48 +0100

    tp5 end

--- tp5.py
+++ tp5.py
@@ -5,7 +5,7 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
 from sklearn.mixture import BayesianGaussianMixture
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.model_selection import train_test_split
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 C = np.array(np.genfromtxt('choixprojetstab.csv',
@@ -16,46 +16,61 @@ M = np.array(np.genfromtxt('choixprojetstab.csv',
 color = ['red','c','green']
 """
 Find best n clusters
+
+
+
 """
 
-x_train, x_test = train_test_split(M, test_size=0.3)
 
-print(x_train, x_test)
 
-bayes = BayesianGaussianMixture(n_components=3).fit(x_train)
-kmean = KMeans(n_clusters=3, random_state=0).fit(x_train)
-spectral = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit(x_train)
-agglo = AgglomerativeClustering(n_clusters=3).fit(x_train)
-
-print(bayes)
-print(kmean)
-print(spectral)
-print(agglo)
-
+bayes = BayesianGaussianMixture(n_components=3).fit(M)
+kmean = KMeans(n_clusters=3, random_state=0).fit(M)
+spectral = SpectralClustering(n_clusters=3, assign_labels="discretize", random_state=0).fit(M)
+agglo = AgglomerativeClustering(n_clusters=3).fit(M)
 
 plt.figure()
 plt.subplot(2,2,1)
 plt.title("Bayes")
-plt.scatter(PCA(n_components=2).fit_transform(x_test)[:,0], PCA(n_components=2).fit_transform(x_test)[:,1],
-            c=np.array(color)[bayes.fit_predict(x_test)], alpha=0.8)
+plt.scatter(PCA(n_components=2).fit_transform(M)[:,0], PCA(n_components=2).fit_transform(M)[:,1],
+            c=np.array(color)[bayes.fit_predict(M)], alpha=0.8)
 plt.subplot(2,2,2)
 plt.title("Kmean")
-plt.scatter(PCA(n_components=2).fit_transform(x_test)[:,0], PCA(n_components=2).fit_transform(x_test)[:,1],
-            c=np.array(color)[kmean.fit_predict(x_test)], alpha=0.8)
+plt.scatter(PCA(n_components=2).fit_transform(M)[:,0], PCA(n_components=2).fit_transform(M)[:,1],
+            c=np.array(color)[kmean.fit_predict(M)], alpha=0.8)
 plt.subplot(2,2,3)
 plt.title("Spectral")
-plt.scatter(PCA(n_components=2).fit_transform(x_test)[:,0], PCA(n_components=2).fit_transform(x_test)[:,1],
-            c=np.array(color)[spectral.fit_predict(x_test)], alpha=0.8)
+plt.scatter(PCA(n_components=2).fit_transform(M)[:,0], PCA(n_components=2).fit_transform(M)[:,1],
+            c=np.array(color)[spectral.fit_predict(M)], alpha=0.8)
 plt.subplot(2,2,4)
 plt.title("Agglo")
-plt.scatter(PCA(n_components=2).fit_transform(x_test)[:,0], PCA(n_components=2).fit_transform(x_test)[:,1],
-            c=np.array(color)[agglo.fit_predict(x_test)], alpha=0.8)
+plt.scatter(PCA(n_components=2).fit_transform(M)[:,0], PCA(n_components=2).fit_transform(M)[:,1],
+            c=np.array(color)[agglo.fit_predict(M)], alpha=0.8)
 plt.show()
 
 
-
+"""
 print("Affichage")
-print(bayes.fit_predict(x_test))
-print(kmean.fit_predict(x_test))
-print(spectral.fit_predict(x_test))
-print(agglo.fit_predict(x_test))
+print(bayes.fit_predict(M))
+print(kmean.fit_predict(M))
+print(spectral.fit_predict(M))
+print(agglo.fit_predict(M))
+"""
+
+
+
+a = bayes.fit_predict(M)
+b = kmean.fit_predict(M)
+c = spectral.fit_predict(M)
+d = agglo.fit_predict(M)
+
+r = np.array([[i,j,k,l] for i,j,k,l in zip(a,b,c,d)])
+abcd = np.array([[np.sum(ij == ji) for ij in r] for ji in r])
+
+
+matrix_simi = cosine_similarity(r)
+cltEnd = SpectralClustering(n_clusters=3, random_state=0, affinity='precomputed')
+cltEnd.fit(matrix_simi)
+plt.figure("Resultat de fin")
+plt.scatter(PCA(n_components=2).fit_transform(M)[:,0], PCA(n_components=2).fit_transform(M)[:,1],
+            c=np.array(color)[cltEnd.fit_predict(matrix_simi)], alpha=0.8)
+plt.show()
