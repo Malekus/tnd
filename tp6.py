f50b9837689d7a3463bc196b22393d80c3f93fd3
Malekus <malekus@outlook.fr>
2018-12-10 17:11:29 +0100

    almost done

--- tp6.py
+++ tp6.py
@@ -1,11 +1,14 @@
 import numpy as np
-import pandas as pd
 from sklearn import datasets
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 
 iris = datasets.load_iris()
 x = datasets.load_iris().data
@@ -17,16 +20,13 @@ class Analyse:
         self.y = data.target
         self.features = data.feature_names
         self.color = np.array(['royalblue', 'g', 'darkorange'])
-        self.pd = pd.DataFrame(data=np.column_stack((data.data,data.target_names[data.target])), columns=[ s.replace(' ', '_') for s in data.feature_names]+["label"])
-
-# sns.load_dataset("iris")
 
     def analyse(self):
-        print("Le nombre de données est " + str(self.x.size))
+        print("Le nombre de donnees est " + str(self.x.size))
         print("Le nombre de variable est " + str(self.x.shape[1]))
         print("Les numeros de classes sont ")
 
-    def visualisation(self):
+    def vis(self):
         pca = PCA(n_components=2)
         lda = LDA(n_components=2)
         plt.figure("Visualisation")
@@ -40,31 +40,53 @@ class Analyse:
                     c=y)
         plt.show()
 
-    def varVis(self):
-        """
-        fig, axs = plt.subplots(len(self.features), len(self.features))
-        for k, a in enumerate(self.features):
-            for l, b in enumerate(self.features):
-                if k == l :
-                    axs[k, l].plot(self.x[:,k], c='y', alpha=0.8)
-                    axs[k, l].plot(self.x[:,l], c='r', alpha=0.8)
-                else :
-                    axs[k, l].scatter(self.x[:,l], self.x[:,k], c=self.color[self.y], alpha=0.8)
-        """
-        g = sns.pairplot(self.pd, hue="label")
+    def varVis(self):       
+        m = 0
+        plt.figure()
+        for ligne in range(0, len(self.features)):
+            for colonne in range(0, len(self.features)):
+                if ligne != colonne:
+                    m = m + 1
+                    plt.subplot(len(self.features), len(self.features)-1, m)
+                    plt.scatter(x=self.x[:,colonne], y=self.x[:,ligne], c=self.color[self.y], alpha=0.8)
+                    plt.xlabel(self.features[colonne])
+                    plt.ylabel(self.features[ligne])
         plt.show()
 
     def regression_lin(self):
-        lm = LinearRegression().fit(self.x, self.y)
-        data = lm.predict(self.x)
+        m = 0
         plt.figure()
-        plt.plot(self.y, data, ' .')
-        x = np.linspace(0, 330, 100)
-        y = x
-        plt.plot(x, y)
-        #plt.show()
+        for ligne in range(0, len(self.features)):
+            for colonne in range(0, len(self.features)):
+                if ligne != colonne:
+                    m = m + 1
+                    plt.subplot(len(self.features), len(self.features)-1, m)
+                    plt.scatter(x=self.x[:,colonne], y=self.x[:,ligne], c=self.color[self.y], alpha=0.8)
+                    plt.xlabel(self.features[colonne])
+                    plt.ylabel(self.features[ligne])                    
+                    x, y = self.makeRegressionLinear(np.array([self.x[:,colonne]]).transpose(), self.x[:, ligne])
+                    plt.plot(x,y, c='r')            
+        plt.show()
         
-        # markers=["o", "s", "D"]
+    def makeRegressionLinear(self, x, y):
+        reglin = LinearRegression()
+        reglin.fit(x, y)
+        r = list(sorted(x))
+        return r, reglin.predict(r)
+    
+    
+    def classification(self, k=3, pourcentage=0.4):
+        X_train, X_test, y_train, y_test = train_test_split(self.x, self.y,
+                                                            test_size=pourcentage, 
+                                                            random_state=0)
+        KNN = KNeighborsClassifier(n_neighbors=k)
+        res = []
+        for index, data  in enumerate(self.x):
+            KNN.fit(np.delete(self.x, index, axis=0), np.delete(self.y, index, axis=0))
+            res.append(KNN.predict([data])[0])
+        
+        return res
+        
         
 a = Analyse(iris)
-a.varVis()
+a.classification()
