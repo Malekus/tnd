4f09af7ab79d45b7d9a0854956c6d078b4e4ab83
ChefDeProjet <abdelkader.moussa@outlook.fr>
2018-12-11 01:15:37 +0100

    tp6 almost done

--- tp6.py
+++ tp6.py
@@ -9,8 +9,23 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+from sklearn.cluster import MeanShift
+from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import SpectralClustering
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import classification_report
+import warnings
+from sklearn.metrics import davies_bouldin_score
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import adjusted_mutual_info_score
+from sklearn.metrics import adjusted_rand_score
+
+
 
 iris = datasets.load_iris()
+wine = datasets.load_wine()
+digits = datasets.load_digits()
+
 x = datasets.load_iris().data
 y = datasets.load_iris().target
 
@@ -20,6 +35,7 @@ class Analyse:
         self.y = data.target
         self.features = data.feature_names
         self.color = np.array(['royalblue', 'g', 'darkorange'])
+        self.target_names = data.target_names if data.target_names is not None else None
 
     def analyse(self):
         print("Le nombre de donnees est " + str(self.x.size))
@@ -75,18 +91,73 @@ class Analyse:
         return r, reglin.predict(r)
     
     
-    def classification(self, k=3, pourcentage=0.4):
-        X_train, X_test, y_train, y_test = train_test_split(self.x, self.y,
-                                                            test_size=pourcentage, 
-                                                            random_state=0)
-        KNN = KNeighborsClassifier(n_neighbors=k)
-        res = []
+    def classifications(self):
+        return [self.classificationModel('KNN'),
+        self.classificationModel('CBN'),
+        self.classificationModel('MLP'),
+        self.classificationModel('SVC')] 
+
+    def classificationModel(self, model='KNN'):
+        if model == 'KNN':
+            clf = KNeighborsClassifier()
+        elif model == 'CBN':
+            clf = GaussianNB()
+        elif model == 'MLP':
+            warnings.simplefilter("ignore")
+            clf = MLPClassifier()
+        elif model == 'SVC':
+            clf = SVC(gamma='auto')
+        else :
+            return
+        tab = []
         for index, data  in enumerate(self.x):
-            KNN.fit(np.delete(self.x, index, axis=0), np.delete(self.y, index, axis=0))
-            res.append(KNN.predict([data])[0])
-        
-        return res
-        
-        
+            clf.fit(np.delete(self.x, index, axis=0), np.delete(self.y, index, axis=0))
+            tab.append(clf.predict([data])[0])
+        return tab, clf.score(self.x, self.y), classification_report(self.y, tab, target_names=self.target_names)
+
+    def clusterings(self):
+        return [self.clusteringModel('MS'), self.clusteringModel('AP'),
+                self.clusteringModel('SC'), self.clusteringModel('DB')]
+    
+    def clusteringModel(self, model='MS', k=3):
+        if model == 'MS':
+            clf = MeanShift().fit(self.x)
+            return clf.labels_, self.makeScoring(clf.labels_)
+        elif model == 'AP':
+            clf = AffinityPropagation().fit(self.x)
+            return clf.labels_, self.makeScoring(clf.labels_)
+        elif model == 'SC':
+            clf = SpectralClustering().fit(self.x)
+            return clf.labels_, self.makeScoring(clf.labels_)
+        elif model == 'DB':
+            clf = DBSCAN()
+            return clf.fit_predict(self.x), self.makeScoring(clf.fit_predict(self.x))
+        else :
+            return
+
+    def makeScoring(self, y):
+        warnings.simplefilter("ignore")
+        a = davies_bouldin_score(self.x, y)
+        b = silhouette_score(self.x, y)
+        c = adjusted_mutual_info_score(self.y, y)
+        d = adjusted_rand_score(self.y, y)
+
+        return np.array([a, b, c, d])
 a = Analyse(iris)
-a.classification()
+b = Analyse(wine)
+c = Analyse(digits)
+
+"""
+from sklearn.metrics import davies_bouldin_score (X, label)
+from sklearn.metrics import silhouette_score (X, label)
+from sklearn.metrics import adjusted_mutual_info_score (labels_true, labels_pred, average_method=’warn’)
+from sklearn.metrics import adjusted_rand_score  (labels_true, labels_pred)
+"""
+
+
+
+
+
+
+
+
